f017c58b630b03364beca676882a6c86b5b847be
Harjas Gill <harjas.g2001@gmail.com>
2023-08-25 21:17:21 +1000

    first commit

---
+++ Chatbot_openai.py
@@ -0,0 +1,7 @@
+#Chatbot_GPT_verson
+
+#Toa access openai, download the libary 
+import openai as ai
+
+#Link chatbot with GPT interface via API key
+ai.api_key = "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"
